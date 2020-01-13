8fd8fac67cf33dcd5622a97b8f2aa4a9aa1ee1b5
Vitor Buzinari <vitorqt2hotmail.com>
2020-01-12 22:18:44 -0300

    Adiciona navbar em projeto

---
+++ src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav>
+    <div class="leftSide">
+      <span>Blitz</span>
+      <span>Escolha um Jogo</span>
+    </div>
+    <div class="rightSide">
+      <ul>
+        <li>
+          <a class="language" href="#">Português</a>
+        </li>
+        <li>
+          <a class="logIn" href="#">Entrar</a>
+        </li>
+        <li>
+          <a class="downloadApp" href="#">Baixe o App</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #0e1726;
+
+  .leftSide {
+    margin-left: 30px;
+
+    span:first-child {
+      font-weight: bold;
+      font-size: 20px;
+      text-transform: uppercase;
+      letter-spacing: 0.3em;
+      color: #fff;
+    }
+    span:last-child {
+      margin-left: 50px;
+      color: #BBC9E0;
+      font-weight: normal;
+    }
+  }
+
+  .rightSide {
+    margin-right: 30px;
+  }
+
+  ul li {
+    display: inline-block;
+    margin-left: 30px;
+
+    .language { color: #BBC9E0; }
+    .logIn { color: #FF7272 }
+    .downloadApp {
+      padding: 10px;
+      border-radius: 5px;
+      background-color: #FF5859;
+      color: #E5EBEF;
+    }
+
+    a { 
+      text-decoration: none; 
+    }
+  }
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="main">
+  <Navbar />
+</div>
+
+
+</template>
+
+<script>
+import Navbar from '@/components/Navbar';
+
+export default {
+  name: "main",
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
